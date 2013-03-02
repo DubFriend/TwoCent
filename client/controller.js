fe6507ee92d240b6bfb192a6faf7a4eee13ef4f0
Brian Detering <brian@brian-ThinkPad-T61p.(none)>
2013-03-02 03:48:43 -0800

    refactored controller into one class

--- client/controller.js
+++ client/controller.js
@@ -1,75 +1,42 @@
-var controller = {
-    
-};
+var new_controller = function (spec) {
+    var that = {},
+        commentModel = spec.commentModel,
+        mainFormModel = spec.mainFormModel,
+        responseFormModel = spec.responseFormModel,
+        responseFormView = spec.responseFormView,
 
-
-var new_comment = function (spec) {
-    var that = Object.create(controller);
-        model = spec.model,
-        view = spec.view,
-        responseFormController = spec.responseFormController || {
-            bind_response_buttons: function () {}
-        },
         bind_more_comments = function () {
             $(window).scroll(function () {
                 if($(window).scrollTop() + get_window_height() >= $(document).height() - 25) {
-                    model.get_next_comments(function () {
-                        responseFormController.bind_response_buttons();
+                    commentModel.get_next_comments(function () {
+                        bind_response_buttons();
                     });
                 }
+            });
+        },
+
+        bind_form = function (formId, formModel) {
+            $(formId).submit(function(e) {
+                e.preventDefault();
+                formModel.submit_comment(function () {
+                    bind_response_buttons();
+                });
+            });
+        },
+
+        bind_response_buttons = function () {
+            var that = this;
+            $("#twocent .response_button").click(function() {
+                var id = $(this).parent().attr("id").slice(3);
+                responseFormView.update({set:id});
+                bind_form("#tc_response_form", responseFormModel);
             });
         };
 
     that.init = function () {
         bind_more_comments();
-    }
-
-    return that;
-};
-
-
-var new_form = function (spec) {
-    var that = Object.create(controller),
-        formModel = spec.formModel,
-        formView = spec.formView,
-        formId = spec.formId,
-        responseFormController = spec.responseFormController || {
-            bind_response_buttons: function () {}
-        };
-
-    that.bind_response_buttons = function () {
-        var that = this;
-        $("#twocent .response_button").click(function() {
-            var id = $(this).parent().attr("id").slice(3);
-            formView.update({set:id});
-            that.bind_form();
-        });
-    };
-
-    that.bind_form = function () {
-        $(formId).submit(function(e) {
-            e.preventDefault();
-            formModel.submit_comment(function () {
-                that.bind_response_buttons();
-            });
-        });
-    };
-
-    that.init = function () {
-        that.bind_form();
-    };
-
-    return that;
-};
-
-
-
-var new_response_form = function (spec) {
-    spec = spec || {};
-    var that = new_form(spec);
-
-    that.init = function () {
-        this.bind_response_buttons();
+        bind_form("#tc_main_form", mainFormModel);
+        bind_response_buttons();
     };
 
     return that;
--- client/twocent.js
+++ client/twocent.js
@@ -1,58 +1,40 @@
 $(document).ready(function () {
 
-
-
     var commentModel = new_comment_model(),
         commentView = new_comment_view(),
-        comment,
 
-        mainFormModel = new_main_form_model(/*{formId: "#tc_main_form"}*/),
+        mainFormModel = new_main_form_model(),
         mainFormView = new_main_form_view(),
-        mainForm,
-
-        responseFormModel = new_response_form_model(/*{formId: "#tc_response_form"}*/),
+        
+        responseFormModel = new_response_form_model(),
         responseFormView = new_response_form_view(),
-        responseForm;
+
+        controller;
 
     commentView.init();
     commentModel.init();
 
     mainFormModel.init();
     mainFormView.init();
-    mainFormModel.subscribe(mainFormView);
-    mainFormModel.subscribe(commentView);
-
 
     responseFormModel.init();
     responseFormView.init();
+
+    mainFormModel.subscribe(mainFormView);
+    mainFormModel.subscribe(commentView);
+    
     responseFormModel.subscribe(responseFormView);
     responseFormModel.subscribe(commentView);
-    responseForm = new_response_form({
-        formId: "#tc_response_form",
-        formModel: responseFormModel,
-        formView: responseFormView
-    });
-
-    responseForm.init();
-
-
-
-    commentModel.subscribe(commentView);
-    comment = new_comment({
-        model: commentModel,
-        view: commentView,
-        responseFormController: responseForm
-    });
-    comment.init();
-
-
-    mainForm = new_form({
-        formId: "#tc_main_form",
-        formModel: mainFormModel,
-        formView: responseFormView
-        //responseFormController: responseForm
-    });
     
-    mainForm.init();
+    commentModel.subscribe(commentView);
+
+    controller = new_controller({
+        commentModel: commentModel,
+        mainFormModel: mainFormModel,
+        responseFormModel: responseFormModel,
+        responseFormView: responseFormView,
+    });
+
+    controller.init();
 
 });
